75f3d2b32036017ec1ec141174ba39a6bdc88473
itsindigo <matthewbridgesbusiness@gmail.com>
2016-03-10 16:30:04 +0000

    Created usersearch controller with fetch method and untested get request to github API

--- public/javascripts/devhubapp.js
+++ public/javascripts/devhubapp.js
@@ -1,1 +1,15 @@
-var devhubApp = angular.module("devhubApp", []);
+(function(){
+  var devhubApp = angular.module("devhubApp", []);
+
+  function fetch() {
+      $http.get('https://api.github.com/users/' + $scope.search)
+      .then(successCallback, errorCallback);
+
+  };
+
+  devhubApp.controller('UserSearchController', function($scope, $http){
+    $scope.$watch('search', function(){
+      fetch();
+    });
+  });
+});
--- test/e2e/scenario.js
+++ test/e2e/scenario.js
@@ -8,14 +8,14 @@ describe('Devhub App', function() {
     browser.get('/');
   });
 
-  // it ("should have a user displayed on the first page", function() {
-  //   var allUsers = element.all(by.repeater('user in users'));
-  //   expect(allUsers.count).not.toBe(0);
-  //   expect(element(by.id('.user_name')));
-  // });
+  it ("should have a user displayed on the first page", function() {
+    var allUsers = element.all(by.repeater('user in users'));
+    expect(allUsers.count).not.toBe(0);
+    expect(element(by.id('.user_name')));
+  });
 
   it("should have a search bar", function() {
-    expect(element(by.id('#title')).isDisplayed());
+    expect(element(by.id('title')).isDisplayed());
   });
 
 });
